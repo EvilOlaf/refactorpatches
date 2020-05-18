14b9a653d4db8c76d67e45273739b4213d688dce
Werner <werner@armbian.de>
2020-05-18 11:01:01 +0200

    Update

--- refactor.py
+++ refactor.py
@@ -1,21 +1,25 @@
 """
 Code that has been copied from the www is mentioned in the references file.
 Make sure to check it to get credit.
+
 """
 
 import pathlib
 import os
 import re
 
-
+# rewrite for later: add recursive walk through patch directories
+#snipped for later use
+#   for x in os.walk(cwd):
+#       print(x)
+#   3-Tuple: dirpath, dirnames, filenames
 print("Current directory:", pathlib.Path.cwd())
-os.chdir("/home/test/p/patchfolder")
+os.chdir(os.path.join(pathlib.Path.cwd(), "patchfolder"))
 print("Working directory:", pathlib.Path.cwd())
 
 currentDir = pathlib.Path.cwd()
 
 # print(os.listdir()) # list all files/dirs in current directory
-#print()
 
 # Grab files *.patch$ in current dir
 patchfiles = [file for file in os.listdir() 
@@ -28,12 +32,13 @@ patchfiles = [file for file in os.listdir()
 def returnDiff(self):
     difflist = []
     for line in self:
-        if "diff --git" in line:
+        if re.search("^diff\s", line):
+       #if "diff --git" in line:
             difflist.append(line)
-    print("difflist:", difflist)
+    #print("difflist:", difflist) #DEBUG
     return(difflist)
 
-
+"""
 def dumpclean(obj):
     if isinstance(obj, dict):
         for k, v in obj.items():
@@ -50,15 +55,17 @@ def dumpclean(obj):
                 print(v)
     else:
         print(obj)
-
+#dumpclean(d)
+"""
 
 #DEBUG
 #for file in patchfiles:
 #    print(file)
 
+# collect information about patchfiles and diffs
 d = {} #empty dictionary
 for patchfile in patchfiles:
-    print("patchfile:", patchfile)
+    #print("patchfile:", patchfile) 'DEBUG
     with open(patchfile, encoding="utf-8", errors="replace") as file:
         diffs = returnDiff(file)
     d[patchfile] = diffs
@@ -67,22 +74,21 @@ print()
 print()
 
 
+# sort by diff
+sortedd = sorted(d.items(), key=lambda kv: kv[1])
 
-sorted_x = sorted(d.items(), key=lambda kv: kv[1])
+for x in sortedd:
+    print(x)
 
 #print(sorted_x)
 
-
+"""
 for patchfiles,diffs in d.items():
     print(patchfiles)
     print(diffs)
     print()
-
-#dumpclean(d)
-
+"""
 
 
-#snipped for later use
-#for x in os.walk(cwd):
-#    print(x)
-# 3-Tuple: dirpath, dirnames, filenames
+
+
