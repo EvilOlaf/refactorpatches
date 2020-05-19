1507d0188f7d8d373d097a69dd0fc63cb3133186
Werner <werner@armbian.de>
2020-05-19 11:04:07 +0200

    license

--- refactor.py
+++ refactor.py
@@ -1,14 +1,20 @@
 """
-Code that has been copied from the www is mentioned in the references file.
-Make sure to check it to get credit.
+    Refactor patch folders
+    Copyright (C) 2020 Werner
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-# find all patches in directory
-# grab those with multiple targets and split them
-# print a (pretty) table with patches to files linked to each other
-#
-# profit!
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import pathlib
